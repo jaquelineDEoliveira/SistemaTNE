0176ac9234f26ec431c106a94f21371136d800c7
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2016-03-12 20:03:19 -0300

    (Prototipo) Alteração na tela de Cadastro(Adição dos Campos, Adição da Biblioteca da tag SELECT)

---
+++ SistemaTNE/Models/DropTableModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaTNE.Models
+{
+    public class DropTableModel
+    {
+        public string Titulo { get; set; }
+    }
+}
